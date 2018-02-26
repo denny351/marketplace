33a1811b38ef7e70fc4b50f0934cb5e10169bf5e
Denny Park <denny.park93@gmail.com>
2018-02-26 02:27:35 -0800

    uninstall redux devtools package

--- store/store.js
+++ store/store.js
@@ -1,14 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
 import promiseMiddleware from 'redux-promise';
 import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
 
 import reducers from './reducers';
+
+const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+
 
 const makeStore = () => {
 	return createStore(
 		reducers,
-		composeWithDevTools(applyMiddleware(promiseMiddleware, thunk))
+		composeEnhancers(applyMiddleware(promiseMiddleware, thunk))
 	);
 };
 
